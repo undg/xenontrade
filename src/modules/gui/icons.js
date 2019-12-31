0bc335abc1d05c44ba5499cfe48a9f5e2a821c3d
morguldir <morguldir@protonmail.com>
2019-12-31 01:24:46 +0100

    Update icons

--- src/modules/gui/icons.js
+++ src/modules/gui/icons.js
@@ -1,5 +1,4 @@
 const BaseIcons = require("../../resource/icons/baseIcons");
-const UniqueIcons = require("../../resource/icons/uniqueIcons");
 
 class Icons {
   static getIconByName(name) {
@@ -7,19 +6,10 @@ class Icons {
       return BaseIcons[name];
     }
 
-    if(UniqueIcons.hasOwnProperty(name))  {
-      return UniqueIcons[name];
-    }
-
     return null;
   }
 
   static findIconByName(name) {
-    var uniqueIcon = Icons.findUniqueIconByName(name);
-    if(uniqueIcon != null) {
-      return uniqueIcon;
-    }
-
     var baseIcon = Icons.findBaseIconByName(name);
     if(baseIcon != null) {
       return baseIcon;
@@ -30,16 +20,6 @@ class Icons {
 
   static findBaseIconByName(name) {
     var icon = Icons._find(name, BaseIcons);
-
-    if(icon != null) {
-      return icon;
-    }
-
-    return null;
-  }
-
-  static findUniqueIconByName(name) {
-    var icon = Icons._find(name, UniqueIcons);
 
     if(icon != null) {
       return icon;
