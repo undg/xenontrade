17ee5ba083e3feb667ad24a098ceeee15d017a0a
und3rdg <und3rdg@gmail.com>
2020-09-05 04:06:32 +0100

    cleanig poe.js and probably fixing calling py script to many times in onClick event.

--- src/modules/poe.js
+++ src/modules/poe.js
@@ -1,14 +1,10 @@
-const DefaultConfig = require("../resource/defaultConfig");
-const Config = require("electron-store");
-const dotProp = require("dot-prop");
-const cp = require("child-process-es6-promise");
+const { exec } = require("child-process-es6-promise");
 const os = require("os");
 const path = require("path");
 const request = require("request-promise-native");
 const ffi = require("ffi");
-const { shell, clipboard } = require("electron");
+const { clipboard } = require("electron");
 const robot = require("robotjs");
-const child_process = require("child_process")
 
 class PathOfExile {
     /**
@@ -26,18 +22,20 @@ class PathOfExile {
      * Focuses the game on Linux
      */
     static _focusOnLinux() {
-        cp.exec("wmctrl -F -a 'Path of Exile'").catch((error) => {
-            console.error(
+        exec("wmctrl -F -a 'Path of Exile'").catch(() =>
+            alert(
                 "Tried to focus Path of Exile but failed, either wmctrl is not installed or Path of Exile is not running"
-            );
-        });
+            )
+        );
     }
 
     /**
      * Focuses the game on Windows
+     *
+     * return {void}
      */
     static _focusOnWindows() {
-        var user32 = new ffi.Library("user32", {
+        const user32 = new ffi.Library("user32", {
             GetTopWindow: ["long", ["long"]],
             FindWindowA: ["long", ["string", "string"]],
             SetActiveWindow: ["long", ["long"]],
@@ -55,28 +53,24 @@ class PathOfExile {
             SetFocus: ["long", ["long"]],
         });
 
-        var kernel32 = new ffi.Library("Kernel32.dll", {
+        const kernel32 = new ffi.Library("Kernel32.dll", {
             GetCurrentThreadId: ["int", []],
         });
 
-        var winToSetOnTop = user32.FindWindowA(null, "Path of Exile");
-        var foregroundHWnd = user32.GetForegroundWindow();
-        var currentThreadId = kernel32.GetCurrentThreadId();
-        var windowThreadProcessId = user32.GetWindowThreadProcessId(
+        const winToSetOnTop = user32.FindWindowA(null, "Path of Exile");
+        const foregroundHWnd = user32.GetForegroundWindow();
+        const currentThreadId = kernel32.GetCurrentThreadId();
+        const windowThreadProcessId = user32.GetWindowThreadProcessId(
             foregroundHWnd,
             null
-        );
-        var showWindow = user32.ShowWindow(winToSetOnTop, 9);
-        var setWindowPos1 = user32.SetWindowPos(winToSetOnTop, -1, 0, 0, 0, 0, 3);
-        var setWindowPos2 = user32.SetWindowPos(winToSetOnTop, -2, 0, 0, 0, 0, 3);
-        var setForegroundWindow = user32.SetForegroundWindow(winToSetOnTop);
-        var attachThreadInput = user32.AttachThreadInput(
-            windowThreadProcessId,
-            currentThreadId,
-            0
-        );
-        var setFocus = user32.SetFocus(winToSetOnTop);
-        var setActiveWindow = user32.SetActiveWindow(winToSetOnTop);
+        )
+        user32.ShowWindow(winToSetOnTop, 9);
+        user32.SetWindowPos(winToSetOnTop, -1, 0, 0, 0, 0, 3);
+        user32.SetWindowPos(winToSetOnTop, -2, 0, 0, 0, 0, 3);
+        user32.SetForegroundWindow(winToSetOnTop);
+        user32.AttachThreadInput(windowThreadProcessId, currentThreadId, 0);
+        user32.SetFocus(winToSetOnTop);
+        user32.SetActiveWindow(winToSetOnTop);
     }
 
     /**
@@ -87,34 +81,23 @@ class PathOfExile {
      * @reject {Error} - The `error.message` contains information about why the promise was rejected
      */
     static getLeagues() {
-        return new Promise(function (resolve, reject) {
+        return new Promise((resolve, reject) => {
             request("http://api.pathofexile.com/leagues?type=main", {
                 json: true,
                 headers: { Connection: "keep-alive" },
             })
-                .then((body) => {
-                    var leagues = [];
-                    var leaguesCount = 0;
+                .then((leagues) => {
                     // Iterate through each league
-                    for (var i = 0; i < body.length; i++) {
-                        var league = body[i];
-                        var ssf = false;
-                        leaguesCount++;
-
-                        if (!PathOfExile._isSoloLeague(league)) {
-                            leagues.push(league.id);
-                        }
-
-                        // When done with every league
-                        if (leaguesCount === body.length) {
-                            resolve(leagues);
-                        }
-                    }
+                    resolve(
+                        leagues
+                        .filter((league) => !PathOfExile._isSoloLeague(league))
+                        .map((league) => league.id)
+                    )
                 })
                 .catch((error) => {
-                    reject(error);
-                });
-        });
+                    reject(error)
+                })
+        })
     }
 
     /**
@@ -135,19 +118,15 @@ class PathOfExile {
             // Send chat message if PoE is focused
             if (app.poeFocused && clipboard.readText() === message) {
                 clearInterval(interval);
-                robot.setKeyboardDelay(50);
+                robot.setKeyboardDelay(35);
                 macroCallback();
 
                 if (send) {
-                    robot.keyToggle("enter", "down");
-                    robot.keyToggle("enter", "up");
+                    robot.tap("enter")
                 }
 
                 // Restore clipboard content
-                let timeout = setTimeout(
-                    () => clipboard.writeText(previousClipboard),
-                    100
-                );
+                setTimeout(() => clipboard.writeText(previousClipboard), 50);
             }
 
             // Clear interval if focusing Path of Exile is taking too long
@@ -170,15 +149,11 @@ class PathOfExile {
         PathOfExile.sendMakro(
             message,
             () => {
-                robot.keyToggle("enter", "up");
-                robot.keyToggle("enter", "down");
-                robot.keyToggle("enter", "up");
-                robot.keyToggle("control", "down");
-                robot.keyToggle("a", "down");
-                robot.keyToggle("a", "up");
-                robot.keyToggle("v", "down");
-                robot.keyToggle("v", "up");
-                robot.keyToggle("control", "up");
+                robot.tap("enter")
+                robot.keyToggle("control", "down")
+                robot.tap("a")
+                robot.tap("v")
+                robot.keyToggle("control", "up")
             },
             send
         );
@@ -195,10 +170,8 @@ class PathOfExile {
             itemName,
             () => {
                 robot.keyToggle("control", "down");
-                robot.keyToggle("f", "down");
-                robot.keyToggle("f", "up");
-                robot.keyToggle("v", "down");
-                robot.keyToggle("v", "up");
+                robot.tap("f");
+                robot.tap("v");
                 robot.keyToggle("control", "up");
             },
             send
@@ -206,34 +179,29 @@ class PathOfExile {
     }
 
     /**
-     * Run any external shell command
+     * Run shell script that grabbing highlighted items from stash.
      *
-     * @param {string} script name, with path if is not in system path
+     * @return {void}
      */
     static grabFromStash() {
-        const pyPath = path.join(process.resourcesPath, '/py/autofire/')
-        const pyScript = 'pick_highlighted.py'
-        const command = 'cd '+ pyPath + ';python '+ pyScript
-        const error = err => {!!err && alert(err)}
+        const pyPath = path.join(process.resourcesPath, "/py/autofire/");
+        const pyScript = "pick_highlighted.py";
+        const command = "python " + pyScript;
 
-        child_process.exec(command, error)
+        exec(command, {
+            cwd: pyPath,
+        })
+        // .then(res=>alert(res.stdout))
+            .catch((err) => alert(err));
     }
 
     /**
-     * Returns `true` if the league rules have a solo rules
+     * Returns `true` for solo self found league
      *
      * @return {boolean}
      */
     static _isSoloLeague(league) {
-        if (league.rules.length > 0) {
-            for (var j = 0; j < league.rules.length; j++) {
-                if (league.rules[j].name === "Solo") {
-                    return true;
-                }
-            }
-        }
-
-        return false;
+        return league.rules.find((rule) => rule.name === "Solo") || false;
     }
 }
 
